70c04aca5f3d5c97e929cf8ac1e77e03ab4a67a3
jaki48 <hallo@jakob48.de>
2025-03-25 14:50:14 +0000

    Füge JSON-Datenverarbeitung in index.php hinzu und entferne die Datenbankverbindung

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $dbname = "gaestebuch";
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+    $entry_file = 'entry.json';
+    $entry = json_decode(file_get_contents($entry_file), true);
+    
+    foreach ($entry as $key => $value) {
+        echo $key . ': ' . $value . '<br>';
     }
-    echo "Connected successfully";
 ?>
